e049650fd746b508b13f984de9e41983abbb999c
Max <jdaimax@gmail.com>
2018-07-16 23:56:41 -0400

    Established backend and frontend communication

--- backend/server.py
+++ backend/server.py
@@ -1,26 +1,26 @@
-from flask import Flask
+from flask import Flask, Response, request
 from fetchData import fetch
 from updateDatabase import update
 import json
 
 app = Flask(__name__)
 
-@app.route('/output')
+@app.route('/output', methods = ['GET'])
 def output():
 	data = fetch()
 	resp = Response(json.dumps(data))
 	resp.headers['Access-Control-Allow-Origin'] = '*'
 	return resp
 
-@app.route('/input')
-	def input():
-		component = request.args.get("component")
-		state = request.args.get("state")
-		update(component, state)
-		answer = [True]
-		resp =  Response(json.dumps(answer))
-		resp.headers['Access-Control-Allow-Origin'] = '*'
-		return resp
+@app.route('/input', methods = ['GET'])
+def input():
+	component = request.args.get("component")
+	state = request.args.get("state")
+	update(component, state)
+	answer = [True]
+	resp =  Response(json.dumps(answer))
+	resp.headers['Access-Control-Allow-Origin'] = '*'
+	return resp
     
 if __name__ == "__main__":
-	app.run(0,0,0,0, threaded=True)
+	app.run('0.0.0.0', threaded=True)
--- backend/updateDatabase.py
+++ backend/updateDatabase.py
@@ -1,8 +1,8 @@
-import MYSQLdb
+import MySQLdb
 
 def update(component, state):
 	db = MySQLdb.connect("localhost", "max", "password", "HomeAutomation")
 	cursor = db.cursor()
-	cursor.execute("UPDATE status SET %s = %d WHERE ID = 1" %(component, state))
+	cursor.execute("UPDATE status SET %s = %d WHERE ID = 1;" %(component, int(state)))
 	db.commit()
 	db.close()
